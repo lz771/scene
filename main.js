fa31e5e8f9b26f04096cb4b6ca6d3c2f3e64f28b
lz771 <lililz7437@gmail.com>
2025-05-12 19:26:03 -0400

    Update main.js

--- main.js
+++ main.js
@@ -5,30 +5,29 @@ const back_img = new Image();
 const fg_img1 = new Image();
 const fg_img2 = new Image();
 
-// background image
-back_img.onload = () =>{
-    ctx.drawImage(back_img,0,0,1500,900);
-};
-
-// foreground image 1
-fg_img1.onload = () =>{
-    ctx.drawImage(fg_img1,0,0,750,450);
-};
-
-// foreground image 2
-fg_img2.onload = () =>{
-    ctx.drawImage(fg_img2,750,450,800,500);
-};
-
-// image source paths
 back_img.src = "images/background.jpg";
 fg_img1.src = "images/cat.jpeg";
 fg_img2.src = "images/dog.jpg";
 
-// text
-setTimeout(() => {
+// loading all the images
+let imagesLoaded = 0;
+const images = [back_img, fg_img1, fg_img2];
+images.forEach(img => {
+  img.onload = () => {
+    imagesLoaded++;
+    if (imagesLoaded === images.length) {
+        drawScene();
+    }
+  };
+});
+
+// draw scene
+function drawScene(){
+    ctx.drawImage(back_img,0,0,1500,900);
+    ctx.drawImage(fg_img1,0,0,750,450);
+    ctx.drawImage(fg_img2,750,450,800,500);
     ctx.font = "60px Arial";
     ctx.fillStyle = "yellow";
     ctx.fillText("Animals", 260, 700);
     ctx.fillText("Lili Lin Zhao", 970, 240);
-}, 500);
+}
